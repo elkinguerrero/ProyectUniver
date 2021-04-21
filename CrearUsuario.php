ba75c65b85f0182b47c255e9da174524bea2f073
elkinguerrero <elkin.guerrero@glocation.com.co>
2021-04-20 23:54:01 -0500

    u

--- CrearUsuario.php
+++ CrearUsuario.php
@@ -5,7 +5,7 @@
     $Correo = $_GET["Correo"];
     $Clave = $_GET["Clave"];
 
-    $query = "INSERT INTO `Clientes`(`Nombre`, `Correo`, `Clave`, `FechaCreacion`) VALUES ('$Nombre','$Correo','$Clave','$FechaCreacion')";
+    $query = "INSERT INTO `Clientes`(`Nombre`, `Correo`, `Clave`) VALUES ('$Nombre','$Correo','$Clave')";
     mysqli_fetch_array($conexion, $query);
     /*while ($resultado = mysqli_fetch_array($query)){
         echo json_decode($resultado);
