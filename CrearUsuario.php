6b79dae861d2cf1518b15ebc733dbf479b2ba407
elkinguerrero <70663113+elkinguerrero@users.noreply.github.com>
2021-04-21 12:12:46 -0500

    u

--- CrearUsuario.php
+++ CrearUsuario.php
@@ -1,18 +1,36 @@
 <?php
-    //phpinfo();
-    //return;
     include("conexion.php");
+    $validar = 0;
 
     $Nombre = $_POST["Nombre"];
     $Correo = $_POST["Correo"];
     $Clave = $_POST["Clave"];
 
-    $query = "INSERT INTO `Clientes`(`Nombre`, `Correo`, `Clave`) VALUES ('$Nombre','$Correo','$Clave')";
+    $query = "SELECT count(*) FROM `Clientes` WHERE `Correo` = ''";
     $resultado = mysqli_query($conexion, $query);
-
     if (!$resultado) {
+        echo "Error al consultar usuario contacte con el administrador\n\n";
         die('Consulta no válida: ' . mysqli_error());
+    }else{
+        while ($resultado = mysqli_fetch_array($query)){
+            $validar = 1;
+            echo "Usuario ya existe en la base de datos";
+        {
     }
+    
+    if($validar == 0){
+        $query = "INSERT INTO `Clientes`(`Nombre`, `Correo`, `Clave`) VALUES ('$Nombre','$Correo','$Clave')";
+        $resultado = mysqli_query($conexion, $query);
+    
+        if (!$resultado) {
+            echo "Error al insertar usuario, contacte con el administrador\n\n";
+            die('Consulta no válida: ' . mysqli_error());
+        }else{
+            echo "Datos insertados correctamente";
+        }
+    }
+
+    
     /*while ($resultado = mysqli_fetch_array($query)){
         echo json_decode($resultado);
     {*/
