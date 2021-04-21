b73a80df62f81d2158912d30000cd8cd5a9122b1
elkinguerrero <70663113+elkinguerrero@users.noreply.github.com>
2021-04-21 14:38:47 -0500

    u

---
+++ LogueoUsuario.php
@@ -0,0 +1,24 @@
+<?php
+    include("conexion.php");
+    $validar = 0;
+
+    $Correo = isset($_POST["Correo"]) ? $_POST["Correo"]:"" ;
+    $Clave = isset($_POST["Clave"]) ? base64_encode($_POST["Clave"]):"" ;
+
+    if($Clave == '' || $Correo == ''){
+        echo "El usuario y la clave no pueden ser vacias";
+    }else{
+        $query = "SELECT * FROM `Clientes` WHERE `Correo` = '$Clave' AND `Clave` = ''";
+        $resultado = mysqli_query($conexion, $query);
+        if (!$resultado) {
+            echo "Error al consultar usuario contacte con el administrador\n\n";
+            die('Consulta no válida: ' . mysqli_error());
+        }else{
+            if(mysqli_num_rows($resultado) != 0){
+                echo "exito";
+            }else{
+                echo "Error contraseña o usuario incorrecto";
+            }
+        }
+    }
+?>
