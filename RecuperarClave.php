4b009832ffac75097cdcfdf25722524110ce674e
elkinguerrero <70663113+elkinguerrero@users.noreply.github.com>
2021-04-21 13:31:14 -0500

    u

---
+++ RecuperarClave.php
@@ -0,0 +1,32 @@
+<?php
+    include("conexion.php");
+    $validar = 0;
+
+    $Correo = isset($_POST["Correo"]) ? $_POST["Correo"]:"" ;
+
+    if( $Correo == ''){
+        echo "Debe colocar un correo";
+    }else{
+        $query = "SELECT * FROM `Clientes` WHERE `Correo` = '$Correo'";
+        $resultado = mysqli_query($conexion, $query);
+        if (!$resultado) {
+            echo "Error al consultar usuario contacte con el administrador\n\n";
+            die('Consulta no válida: ' . mysqli_error());
+        }else{
+            if(mysqli_num_rows($resultado) == 0){
+                $respuesta = mysqli_fetch_assoc($resultado);
+
+                echo "Se ha enviado un correo con sus datos";
+                //correo
+                include("phpmailer/correo.php");
+                $subject="Recuperacion de clave ProyectoUniversidad";
+                $body="Su clave es: "+base64_decode($respuesta['Clave']);
+                enviar_mensaje([$Correo], $subject, $body);
+                //correo
+            }
+            }else{
+                echo "El correo no esta en nuestra base de datos";
+            }
+        }
+    }
+?>
