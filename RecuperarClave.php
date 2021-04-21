0c6a8c19fe3cefa008327cf9ca8aa288e91e81d3
elkinguerrero <70663113+elkinguerrero@users.noreply.github.com>
2021-04-21 13:40:06 -0500

    u

--- RecuperarClave.php
+++ RecuperarClave.php
@@ -2,7 +2,7 @@
     include("conexion.php");
     $validar = 0;
 
-    $Correo = isset($_POST["Correo"]) ? $_POST["Correo"]:"" ;
+    $Correo = isset($_GET["Correo"]) ? $_GET["Correo"]:"" ;
 
     if( $Correo == ''){
         echo "Debe colocar un correo";
@@ -21,7 +21,7 @@
                 include("phpmailer/correo.php");
                 $subject="Recuperacion de clave ProyectoUniversidad";
                 $body="Su clave es: "+base64_decode($respuesta['Clave']);
-                enviar_mensaje([$Correo], $subject, "asdasdasd");
+                enviar_mensaje([$Correo], $subject, $body);
                 //correo
             }else{
                 echo "El correo no esta en nuestra base de datos";
