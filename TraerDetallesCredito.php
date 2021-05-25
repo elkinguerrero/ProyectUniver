f66b0e715ec1888080261110c9e655a9192c168e
elkinguerrero <70663113+elkinguerrero@users.noreply.github.com>
2021-05-24 21:16:54 -0500

    u

---
+++ TraerDetallesCredito.php
@@ -0,0 +1,34 @@
+<?php
+    include("conexion.php");
+
+    $idcredito = isset($_POST["idcredito"]) ? $_POST["idcredito"]:"" ;
+
+    if($idcredito == ''){
+        echo "Error de servicio, contacte con el administrador";
+    }else{
+        $query = "  SELECT * 
+                    FROM `CreditosPrestamistaUsuarios` A
+                    INNER JOIN `Creditos` B
+                    ON B.Id = A.IdCredito
+                    INNER JOIN `Usuarios` C
+                    ON C.Id = A.IdPrestamista
+                    WHERE A.`IdCredito` = $idcredito";
+                
+
+        $resultado = mysqli_query($conexion, $query);
+        if (!$resultado) {
+            echo "Error al consultar la base de datos contacte con el administrador\n\n";
+            die('Consulta no válida: ' . mysqli_error());
+        }else{
+            if(mysqli_num_rows($resultado) == 0){
+                echo "No hay datos para mostrar";
+            }else{
+                $myArray = array();
+                while ($fila = mysqli_fetch_array($resultado)) {
+                    $myArray[] = $fila;
+                }
+                echo json_encode($myArray);
+            }
+        }
+    }
+?>
