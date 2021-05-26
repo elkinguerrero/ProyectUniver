532f58e446414e5155eb3b770b73b2751a1413b6
elkinguerrero <70663113+elkinguerrero@users.noreply.github.com>
2021-05-26 16:17:07 -0500

    u}

---
+++ TraerUsuarios.php
@@ -0,0 +1,22 @@
+<?php
+    include("conexion.php");
+
+    $query = "  SELECT * FROM `Usuarios` WHERE `Perfli` = 'Usuario'";
+
+    $resultado = mysqli_query($conexion, $query);
+    if (!$resultado) {
+        echo "Error al consultar la base de datos contacte con el administrador\n\n";
+        die('Consulta no válida: ' . mysqli_error());
+    }else{
+        if(mysqli_num_rows($resultado) == 0){
+            echo "No hay datos para mostrar";
+        }else{
+            $myArray = array();
+            while ($fila = mysqli_fetch_array($resultado)) {
+                $myArray[] = $fila;
+            }
+            echo json_encode($myArray);
+        }
+    }
+    
+?>
