43063b008fa9cd6b014c6b830b039ea212952334
elkinguerrero <70663113+elkinguerrero@users.noreply.github.com>
2021-05-26 22:11:59 -0500

    u

---
+++ TraerUsuariosEspecifico.php
@@ -0,0 +1,27 @@
+<?php
+    include("conexion.php");
+
+    $perfil = isset($_POST["perfil"]) ? $_POST["perfil"]:"" ;
+
+    if($perfil == ''){
+        echo "Error de servicio, contacte con el administrador";
+    }else{
+        $query = " SELECT * FROM `Usuarios` WHERE `Id` = '$idusuario'";
+
+        $resultado = mysqli_query($conexion, $query);
+        if (!$resultado) {
+            echo "Error al consultar la base de datos contacte con el administrador\n\n";
+            die('Consulta no válida: ' . mysqli_error());
+        }else{
+            if(mysqli_num_rows($resultado) == 0){
+                echo "No hay datos para mostrar";
+            }else{
+                $myArray = array();
+                while ($fila = mysqli_fetch_array($resultado)) {
+                    $myArray[] = $fila;
+                }
+                echo json_encode($myArray);
+            }
+        }
+    }
+?>
